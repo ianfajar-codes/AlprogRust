78fbef9e3318528b236fea8c0239b09794586b0d
ianfajar-codes <adriantacollege13@gmail.com>
2025-06-26 20:33:46 +0700

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,21 @@
+mod mongo;
+mod model;
+mod app;
+mod compute;
+
+use app::MyApp;
+use eframe::NativeOptions;
+use egui::ViewportBuilder;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = NativeOptions {
+        viewport: ViewportBuilder::default().with_fullscreen(true),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "📈 Dashboard Sensor Gas",
+        options,
+        Box::new(|_cc| Box::new(MyApp::new())),
+    )
+}
